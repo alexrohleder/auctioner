2deea36bbfe0eaca5898ec56e64c0593b8ab582c
Alex Rohleder <a.rohleder@sportradar.com>
2021-05-08 22:37:45 +0200

    add patch method to auction api

--- src/pages/api/v1/auctions/[id]/index.ts
+++ src/pages/api/v1/auctions/[id]/index.ts
@@ -4,27 +4,59 @@ import prisma from "../../../../../lib/db";
 import { HttpError } from "../../../../../lib/errors";
 import validate from "../../../../../lib/validate";
 
-export default api().get(async (req, res) => {
-  const { where } = validate(req.query, {
-    id: Joi.string().uuid().required(),
-  });
+export default api()
+  .get(async (req, res) => {
+    const { where } = validate(req.query, {
+      id: Joi.string().uuid().required(),
+    });
 
-  const auction = await prisma.auction.findFirst({
-    include: {
-      bids: {
-        select: {
-          bidderId: true,
-          value: true,
-          createdAt: true,
+    const auction = await prisma.auction.findFirst({
+      include: {
+        bids: {
+          select: {
+            bidderId: true,
+            value: true,
+            createdAt: true,
+          },
         },
       },
-    },
-    where,
+      where,
+    });
+
+    if (!auction) {
+      throw new HttpError(404);
+    }
+
+    res.json(auction);
+  })
+  .patch(async (req, res) => {
+    const { data } = validate(req.body, {
+      id: Joi.string().uuid().required(),
+      title: Joi.string(),
+      description: Joi.string(),
+      bidIncrement: Joi.number().positive(),
+      isPublished: Joi.bool(),
+      isSettled: Joi.bool(),
+    });
+
+    const auction = await prisma.auction.findUnique({
+      where: {
+        id: data.id,
+      },
+    });
+
+    if (!auction) {
+      throw new HttpError(404);
+    }
+
+    const { id, ...update } = data;
+
+    res.json(
+      await prisma.auction.update({
+        where: {
+          id,
+        },
+        data: update,
+      })
+    );
   });
-
-  if (!auction) {
-    throw new HttpError(404);
-  }
-
-  res.json(auction);
-});
