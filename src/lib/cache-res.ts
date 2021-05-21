a00388a1bbf591effad6c70df2fdac1eea47ec34
Alex Rohleder <a.rohleder@sportradar.com>
2021-05-21 14:11:17 +0200

    change cache res values

--- src/lib/cache-res.ts
+++ src/lib/cache-res.ts
@@ -1,9 +1,13 @@
 import { NextApiResponse } from "next";
 import ms from "ms";
 
-const cacheRes = (res: NextApiResponse, age: string, revalidate: string) => {
+const cacheRes = (
+  res: NextApiResponse,
+  age: string,
+  revalidate: string = ""
+) => {
   const maxAge = ms(age) / 1000;
-  const staleWithRevalidate = ms(revalidate) / 1000;
+  const staleWithRevalidate = revalidate ? ms(revalidate) / 1000 : maxAge / 2;
 
   res.setHeader(
     "Cache-Control",
--- src/pages/api/v1/reviews/index.ts
+++ src/pages/api/v1/reviews/index.ts
@@ -11,7 +11,7 @@ export default api()
   .get(async (req, res) => {
     const { take = 10, skip, ...data } = SelectReviewSchema.parse(req.query);
 
-    cacheRes(res, "1d", "12h");
+    cacheRes(res, "1d");
 
     res.json(
       await getReviews({
